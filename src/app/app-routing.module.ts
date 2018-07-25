6f151c529a1644423940b6a6f3fb272c4a554b79
Xin Tan <mactanxin@gmail.com>
2018-07-25 14:18:53 -0700

    ADDED: routers and routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PokedexComponent } from './components/pokedex/pokedex.component';
+
+
+const routes: Routes = [
+  { path : '', component: PokedexComponent},
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { PokedexComponent } from './components/pokedex/pokedex.component';
 import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PokeDetailComponent } from './components/poke-detail/poke-detail.compon
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
